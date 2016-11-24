c1cc051b1cd2c2a91763affc877d3cc8d8bd43cd
Christian Falch <christian.falch@gmail.com>
2016-11-24 10:10:36 +0100

    Updated build script to use correct paths.

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 #
-nuget pack
+set -e # Exit on any failure for any command
+set -x # Print all commands before running them - for easy debugging.
+
+## Clean
+xbuild /p:Configuration=Release /p:Platform=Release /target:Clean /p:OutputPath=build NControl.MVVM/NControl.Mvvm.csproj
+rm -rf NControl.MVVM/build/
+xbuild /p:Configuration=Release /p:Platform=Release /target:Clean /p:OutputPath=build NControl.MVVM.iOS/NControl.Mvvm.iOS.csproj
+rm -rf NControl.MVVM.iOS/build/
+xbuild /p:Configuration=Release /p:Platform=Release /target:Clean /p:OutputPath=build NControl.MVVM.Droid/NControl.Mvvm.Droid.csproj
+rm -rf NControl.MVVM.Droid/build/
+
+## Build with parameters
+xbuild /p:Configuration=Release /p:Platform=Release /target:build /p:OutputPath=build NControl.MVVM/NControl.Mvvm.csproj
+xbuild /p:Configuration=Release /p:Platform=Release /target:build /p:OutputPath=build NControl.MVVM.iOS/NControl.Mvvm.iOS.csproj
+xbuild /p:Configuration=Release /p:Platform=Release /target:build /p:OutputPath=build NControl.MVVM.Droid/NControl.Mvvm.Droid.csproj
+
+mkdir -p ../build
+nuget pack -OutputDirectory ../build
