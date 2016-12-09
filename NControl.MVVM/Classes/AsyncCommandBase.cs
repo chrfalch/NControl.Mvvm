a03a5a0f639f4866fff6ddfdc6c72627bcf3bad0
Christian Falch <christian.falch@gmail.com>
2016-12-09 15:00:54 +0100

    Added Asyns Commands.

---
+++ NControl.MVVM/Classes/AsyncCommandBase.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Based on the article: Patterns for Asynchronous MVVM Applications: Commands
+ * http://msdn.microsoft.com/en-us/magazine/dn630647.aspx
+ */
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NControl.Mvvm
+{
+	public abstract class AsyncCommandBase : IAsyncCommand
+	{
+		public abstract bool CanExecute(object parameter);
+
+		public abstract Task ExecuteAsync(object parameter);
+
+		public async void Execute(object parameter)
+		{
+			await ExecuteAsync(parameter);
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		protected void RaiseCanExecuteChanged()
+		{
+			CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
---
+++ NControl.MVVM/Classes/IAsyncCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NControl.Mvvm
+{
+	public interface IAsyncCommand : ICommand
+	{
+		Task ExecuteAsync(object parameter);
+	}
+}
