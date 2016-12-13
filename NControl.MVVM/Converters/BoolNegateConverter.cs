023e36e582d744fec0017ebdca7a34f72bffd08a
Christian Falch <christian.falch@gmail.com>
2016-12-13 08:26:04 +0100

    Added value converters

---
+++ NControl.MVVM/Converters/BoolNegateConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace NControl.Mvvm
+{
+	/// <summary>
+	/// Implements a converter that negates the boolean input.
+	/// </summary>
+	public class BoolNegateConverter: IValueConverter
+	{
+		/// <param name="value">To be added.</param>
+		/// <param name="targetType">To be added.</param>
+		/// <param name="parameter">To be added.</param>
+		/// <param name="culture">To be added.</param>
+		/// <summary>
+		/// Convert the specified value, targetType, parameter and culture.
+		/// </summary>
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null)
+				return true;
+
+			return !(bool)value;
+		}
+
+		/// <param name="value">To be added.</param>
+		/// <param name="targetType">To be added.</param>
+		/// <param name="parameter">To be added.</param>
+		/// <param name="culture">To be added.</param>
+		/// <summary>
+		/// Converts the back.
+		/// </summary>
+		/// <returns>The back.</returns>
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null)
+				return true;
+
+			return !(bool)value;
+		}
+	}
+}
---
+++ NControl.MVVM/Converters/EmptyListToBoolConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace NControl.Mvvm
+{
+	/// <summary>
+	/// Returns true if the collection is empty or null, false if it contains any elements. Use
+	/// to bind activity indicators etc. to collections
+	/// </summary>
+	public class EmptyListToBoolConverter : IValueConverter
+	{
+		public EmptyListToBoolConverter()
+		{
+			ReturnValueWhenEmpty = true;
+		}
+
+		public EmptyListToBoolConverter(bool returnValueWhenEmpty)
+		{
+			ReturnValueWhenEmpty = returnValueWhenEmpty;
+		}
+
+		public bool ReturnValueWhenEmpty { get; private set; }
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+				return ReturnValueWhenEmpty;
+
+			if (value is IList)
+				return (value as IList).Count == 0 ? ReturnValueWhenEmpty : !ReturnValueWhenEmpty;
+
+			return ReturnValueWhenEmpty;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
