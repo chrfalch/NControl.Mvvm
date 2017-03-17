4e293c192f8a08c7bc5ef1db1a29521e0baa9a14
Christian Falch <christian.falch@gmail.com>
2017-03-17 09:12:20 +0100

    Moved animation page to own page.

--- Animation/XAnimationDemo/XAnimationDemo.cs
+++ Animation/XAnimationDemo/XAnimationDemo.cs
@@ -8,88 +8,13 @@ namespace XAnimationDemo
 	{
 		public App()
 		{
-			var label = new Label
-			{
-				HorizontalTextAlignment = TextAlignment.Center,
-				Text = "Welcome to Xamarin Forms!"
-			};
-			var slider = new Slider
-			{
-				Minimum = 0,
-				Maximum = 360,
-				Value = 180,
-
-			};
-
-			var slidervalue = new Label
-			{
-				HorizontalTextAlignment = TextAlignment.Center,
-				BindingContext = slider,
-			};
-			slidervalue.SetBinding(Label.TextProperty, nameof(Slider.Value));
-
-			var checkbox = new Switch { HorizontalOptions = LayoutOptions.Center };
-
-			var button = new Button
-			{
-				Text = "Animate!",
-				Command = new Command(() =>
-				{
-
-					Action action = null;
-					action = () =>
-					{
-						new XAnimationPackage(label)
-							.Rotate(0)
-							.Set()
-							.Duration(1000)
-							.Color(Color.Red)
-							.Rotate(slider.Value)
-							.Easing(.07, .62, .58, 1.51)
-							.Animate()
-							.Color(Color.Transparent)
-							.Animate()
-							.Run(() =>
-							{
-								if (checkbox.IsToggled) action();
-							});
-					};
-
-					action();
-				}),
-			};
-
-			// The root page of your application
-			ContentPage contentPage = null;
-			contentPage = new ContentPage
-			{
-				Title = "XAnimationDemo",
-				Content = new StackLayout
-				{
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						label,
-						button,
-						slider,
-						slidervalue,
-						new Button{
-							Text = "More...",
-							Command = new Command(()=> {
-								contentPage.Navigation.PushAsync(new XAnimationDemoPage());
-							})
-						},
-						checkbox,
-					}
-				}
-			};
-
 			MainPage = new NavigationPage(new ContentPage
 			{
 				Content = new Button
 				{
 					Text = "First",
 					Command = new Command((obj) => {
-						(MainPage as NavigationPage).Navigation.PushAsync(contentPage);
+						(MainPage as NavigationPage).Navigation.PushAsync(new XAnimationDemoPage2());
 					})
 				},
 			});
---
+++ Animation/XAnimationDemo/XAnimationDemoPage2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using NControl.XAnimation;
+using Xamarin.Forms;
+namespace XAnimationDemo
+{
+	public class XAnimationDemoPage2: ContentPage
+	{
+		public XAnimationDemoPage2()
+		{
+			var label = new Label
+			{
+				HorizontalTextAlignment = TextAlignment.Center,
+				Text = "Welcome to Xamarin Forms!"
+			};
+			var slider = new Slider
+			{
+				Minimum = 0,
+				Maximum = 360,
+				Value = 180,
+
+			};
+
+			var slidervalue = new Label
+			{
+				HorizontalTextAlignment = TextAlignment.Center,
+				BindingContext = slider,
+			};
+			slidervalue.SetBinding(Label.TextProperty, nameof(Slider.Value));
+
+			var checkbox = new Switch { HorizontalOptions = LayoutOptions.Center };
+
+			var button = new Button
+			{
+				Text = "Animate!",
+				Command = new Command(() =>
+				{
+
+					Action action = null;
+					action = () =>
+					{
+						new XAnimationPackage(label)
+							.Rotate(0)
+							.Set()
+							.Duration(1000)
+							.Color(Color.Red)
+							.Rotate(slider.Value)
+							.Easing(.07, .62, .58, 1.51)
+							.Animate()
+							.Color(Color.Transparent)
+							.Animate()
+							.Run(() =>
+							{
+								if (checkbox.IsToggled) action();
+							});
+					};
+
+					action();
+				}),
+			};
+
+			// The root page of your application
+			Title = "XAnimationDemo";
+			Content = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					label,
+					button,
+					slider,
+					slidervalue,
+					new Button{
+						Text = "More...",
+						Command = new Command(()=> {
+							this.Navigation.PushAsync(new XAnimationDemoPage());
+						})
+					},
+					checkbox,
+				}
+			};
+		}
+	}
+}
