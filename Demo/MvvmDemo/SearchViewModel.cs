af209349794f9f415c62ab59db951fe32e2c086d
Christian Falch <christian.falch@gmail.com>
2017-04-17 10:32:16 +0200

    Removed the RX extensions due to NETPlatform dependeny

--- Demo/MvvmDemo/SearchViewModel.cs
+++ Demo/MvvmDemo/SearchViewModel.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using NControl.Mvvm;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 using System.Linq;
-using System.Reactive.Linq;
 using System.Windows.Input;
 
 namespace MvvmDemo
@@ -15,19 +13,19 @@ namespace MvvmDemo
 		/// </summary>
 		public SearchViewModel ()
 		{
-			this.OnPropertyChanges (d => d.Query)
-				.DistinctUntilChanged ()
-				.Throttle (TimeSpan.FromSeconds (0.25))
-				.Select(t => t.ToLowerInvariant())
-				.Subscribe (_ => {
-					Employees.Clear();
-					if(string.IsNullOrEmpty(Query))
-						Employees.AddRange(Employee.EmployeeRepository);
-					else
-						Employees.AddRange(Employee.EmployeeRepository
-							.Where(mn => mn.Name.ToLowerInvariant()
-								.Contains(Query.ToLowerInvariant())));	
-				});
+			//this.OnPropertyChanges (d => d.Query)
+			//	.DistinctUntilChanged ()
+			//	.Throttle (TimeSpan.FromSeconds (0.25))
+			//	.Select(t => t.ToLowerInvariant())
+			//	.Subscribe (_ => {
+			//		Employees.Clear();
+			//		if(string.IsNullOrEmpty(Query))
+			//			Employees.AddRange(Employee.EmployeeRepository);
+			//		else
+			//			Employees.AddRange(Employee.EmployeeRepository
+			//				.Where(mn => mn.Name.ToLowerInvariant()
+			//					.Contains(Query.ToLowerInvariant())));	
+			//	});
 		}
 
 		/// <summary>
@@ -56,7 +54,17 @@ namespace MvvmDemo
 		/// <value>The query.</value>
 		public string Query {
 			get { return GetValue<string> (); }
-			set { SetValue<string> (value); }
+			set { 
+				SetValue (value); 
+
+				Employees.Clear();
+				if(string.IsNullOrEmpty(Query))
+					Employees.AddRange(Employee.EmployeeRepository);
+				else
+					Employees.AddRange(Employee.EmployeeRepository
+						.Where(mn => mn.Name.ToLowerInvariant()
+							.Contains(Query.ToLowerInvariant())));	
+			}
 		}
 
 		/// <summary>
--- build.sh
+++ build.sh
@@ -11,9 +11,6 @@ rm -rf NControl.MVVM.iOS/build/
 xbuild /p:Configuration=Release /p:Platform=Release /target:Clean /p:OutputPath=build NControl.MVVM.Droid/NControl.Mvvm.Droid.csproj
 rm -rf NControl.MVVM.Droid/build/
 
-xbuild /p:Configuration=Release /p:Platform=Release /target:Clean /p:OutputPath=build NControl.Mvvm.RX/NControl.Mvvm.RX.csproj
-rm -rf NControl.Mvvm.RX/build/
-
 xbuild /p:Configuration=Release /p:Platform=Release /target:Clean /p:OutputPath=build NControl.MVVM.Fluid/NControl.Mvvm.Fluid.csproj
 rm -rf NControl.MVVM.Fluid/build/
 xbuild /p:Configuration=Release /p:Platform=Release /target:Clean /p:OutputPath=build NControl.MVVM.Fluid.iOS/NControl.Mvvm.Fluid.iOS.csproj
@@ -30,10 +27,7 @@ xbuild /p:Configuration=Release /p:Platform=Release /target:build /p:OutputPath=
 xbuild /p:Configuration=Release /p:Platform=Release /target:build /p:OutputPath=build NControl.MVVM.Fluid.iOS/NControl.Mvvm.Fluid.iOS.csproj
 xbuild /p:Configuration=Release /p:Platform=Release /target:build /p:OutputPath=build NControl.MVVM.Fluid.Droid/NControl.Mvvm.Fluid.Droid.csproj
 
-xbuild /p:Configuration=Release /p:Platform=Release /target:build /p:OutputPath=build NControl.Mvvm.RX/NControl.Mvvm.RX.csproj
-
 mkdir -p ../build
 nuget pack -OutputDirectory ../build NControl.MVVM.Fluid.nuspec 
 nuget pack -OutputDirectory ../build NControl.MVVM.nuspec 
-nuget pack -OutputDirectory ../build NControl.MVVM.Reactive.nuspec 
 nuget pack -OutputDirectory ../build Animation/NControl.XAnimation.nuspec 
