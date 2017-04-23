9d2d982f2a17acd9105e17eff2bb30ffc2f5c62a
Christian Falch <christian.falch@gmail.com>
2017-04-23 21:54:06 +0200

    Updated navigation container with overlay support.

--- NControl.MVVM.Fluid/Containers/FluidModalNavigationContainer.cs
+++ NControl.MVVM.Fluid/Containers/FluidModalNavigationContainer.cs
@@ -8,41 +8,43 @@ namespace NControl.Mvvm
 {
 	public class FluidModalNavigationContainer: FluidNavigationContainer
 	{
-		// View _overlay;
-
-		//protected override void AddViewsToBottomOfStack(RelativeLayout layout)
-		//{
-		//	base.AddViewsToBottomOfStack(layout);
-
-		//	_overlay = new BoxView
-		//	{
-		//		BackgroundColor = Config.ViewTransparentBackgroundColor,
-		//	};
-
-		//	layout.Children.Add(_overlay, () => layout.Bounds);
-		//}
+		View _overlay;
 
 		public override View GetOverlayView()
 		{
-			return null; //_overlay;
+			if (_overlay == null)
+			{
+				_overlay = new BoxView
+				{
+					BackgroundColor = Config.ViewTransparentBackgroundColor,
+				};
+			}
+
+			return _overlay;
 		}
 
-		//public override IEnumerable<XAnimationPackage> TransitionIn(
-		//	INavigationContainer fromContainer, PresentationMode presentationMode)
-		//{
-		//	var retVal = new List<XAnimationPackage>(base.TransitionIn(fromContainer, presentationMode));
-		//	retVal.Add(new XAnimationPackage(_overlay).Opacity(0.0).Set().Opacity(1.0).Animate());
-		//	return retVal;
-		//}
+		public override IEnumerable<XAnimationPackage> TransitionIn(
+			INavigationContainer fromContainer, PresentationMode presentationMode)
+		{
+			var retVal = new List<XAnimationPackage>(base.TransitionIn(fromContainer, presentationMode));
+			retVal.Add(new XAnimationPackage(_overlay)
+			           .Opacity(0.0)
+			           .Set()
+			           .Opacity(1.0)
+			           .Animate());
+			
+			return retVal;
+		}
 
-		//public override IEnumerable<XAnimationPackage> TransitionOut(
-		//	INavigationContainer toContainer, PresentationMode presentationMode)
-		//{
-		//	// TODO: This is a hack - dont know why...
-		//	_navigationContainer.IsVisible = false;
-		//	var retVal = new List<XAnimationPackage>(base.TransitionOut(toContainer, presentationMode));
-		//	retVal.Add(new XAnimationPackage(_overlay).Opacity(0.0).Animate());
-		//	return retVal;
-		//}
+		public override IEnumerable<XAnimationPackage> TransitionOut(
+			INavigationContainer toContainer, PresentationMode presentationMode)
+		{
+			var retVal = new List<XAnimationPackage>(base.TransitionOut(toContainer, presentationMode));
+			retVal.Add(new XAnimationPackage(_overlay)
+			           .Opacity(0.0)
+			           .Animate());
+			
+			return retVal;
+		}
 	}
 }
