0defc770666ce0c977244672f484422ac4bd5d05
Christian Falch <christian.falch@gmail.com>
2017-06-12 18:10:38 +0200

    Added opactiy animation for bottom section.

--- Demo/MvvmDemo/CityDetailsView.cs
+++ Demo/MvvmDemo/CityDetailsView.cs
@@ -7,8 +7,10 @@ using Xamarin.Forms;
 namespace MvvmDemo
 {
 	public class CityDetailsView : BaseFluidContentsView<CityDetailsViewModel>, 
-		INavigationContainerProvider, IXViewTransitionable
+	INavigationContainerProvider, IXViewTransitionable
 	{
+		StackLayout _bottomDuplicate;
+
 		public INavigationContainer CreateNavigationContainer(PresentationMode mode)
 		{
 			return new FluidTransitionNavigationContainer(true);
@@ -18,19 +20,27 @@ namespace MvvmDemo
 			string transitionIdentifier, VisualElement fromElement, VisualElement toElement, 
 			Rectangle fromRect, Rectangle toRectangle, XInterpolationPackage package)
 		{
-			var list = new List<XInterpolationPackage>();
-			list.Add(package);
+			return new XInterpolationPackage[] { package };
+		}
 
-			if (transitionIdentifier == "name-details")
-			{
-				// Create new package
-				//list.Add(new XInterpolationPackage(fromElement.Parent as VisualElement)				         
-				//         .Add((transform) => transform.SetDuration(1).SetTranslation(0, (fromElement.Parent as VisualElement).Height))
-				//         .Add((transform) => transform.SetDuration(99).SetTranslation(0, 0)) 
-				//         as XInterpolationPackage);
-			}
+		protected override IEnumerable<XAnimationPackage> DefaultTransitionIn(INavigationContainer container, IEnumerable<XAnimationPackage> animations)
+		{
+			var retVal = new List<XAnimationPackage>(base.DefaultTransitionIn(container, animations));
 
-			return list;
+			retVal.Add(new XAnimationPackage(_bottomDuplicate).Add((t) => t.SetOpacity(0.0))
+			           as XAnimationPackage);
+
+			return retVal;
+		}
+
+		protected override IEnumerable<XAnimationPackage> DefaultTransitionOut(INavigationContainer container, IEnumerable<XAnimationPackage> animations)
+		{
+			var retVal = new List<XAnimationPackage>(base.DefaultTransitionOut(container, animations));
+
+			retVal.Add(new XAnimationPackage(_bottomDuplicate).Add((t) => t.SetOpacity(1.0))
+					   as XAnimationPackage);
+
+			return retVal;
 		}
 
 		protected override View CreateContents()
@@ -61,7 +71,7 @@ namespace MvvmDemo
 					}.SetTransitionIdentifier("name-details", TransitionTarget.Target),
 				}
 			}, 0, 0)
-             .AddChildTo(new VerticalStackLayoutWithSmallPadding{
+             .AddChildTo((_bottomDuplicate = new VerticalStackLayoutWithSmallPadding{
 				 BackgroundColor = Color.Black.MultiplyAlpha(0.5),
 				 VerticalOptions = LayoutOptions.End,
 				 HorizontalOptions = LayoutOptions.FillAndExpand,
@@ -77,7 +87,7 @@ namespace MvvmDemo
 						FontSize = Device.GetNamedSize(NamedSize.Micro, typeof(Label)),
 					}.BindTo(Label.TextProperty, nameof(ViewModel.Name))
 				}
-			},0,0);
+			}),0,0);
 		}
 	}
 }
